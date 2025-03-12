9ae7dc626587ea9606503f9896f86136e87c4825
DevByChoice <danielwonnacott@hotmail.co.uk>
2025-03-12 22:16:33 +0000

    Add project files.

---
+++ Melee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicGame
+{
+    internal class Melee : Enemy
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        float HP { get; set; }
+        float Damage { get; set; }
+    }
+}
